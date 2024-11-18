67e86506c9d2321e23d7761cbb1f3e797185397c
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2024-11-18 07:37:45 +0000

    working on a basic random generation and plotting script, but need to go to bed now

--- scripts/PlotRandomNoise.py
+++ scripts/PlotRandomNoise.py
@@ -1,3 +1,21 @@
+import random
+import numpy as np
 import matplotlib.pyplot as plt
-import icosalattice as ic
 
+from icosalattice.UnitSpherePoint import UnitSpherePoint
+import icosalattice.IcosahedronMath as icm
+
+
+
+if __name__ == "__main__":
+    n_points = 100
+    usps = [UnitSpherePoint.random() for i in range(n_points)]
+    pcs = []
+    for p in usps:
+        spc, l, d = icm.get_peel_coordinates_of_point(p)
+        pc = icm.get_point_code_from_peel_coordinates(spc, l, d)
+        pcs.append(pc)
+    els = [random.uniform(-100, 100) for i in range(n_points)]
+    d = {pc: el for pc, el in zip(pcs, els)}
+    print(d)
+    
