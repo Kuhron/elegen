8d8652a42a0c55fa390e82d22c3445e9ab2d8b74
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2024-12-17 18:59:33 +0000

    various

--- clean_before_build.sh
+++ clean_before_build.sh
@@ -1,5 +1,5 @@
 rm -rf build
 rm -rf dist
 rm -rf *.egg-info
-rm -rf src/icosalattice.egg-info
+rm -rf src/elegen.egg-info
 
--- scripts/PlotRandomNoise.py
+++ scripts/PlotRandomNoise.py
@@ -4,18 +4,21 @@ import matplotlib.pyplot as plt
 
 from icosalattice.UnitSpherePoint import UnitSpherePoint
 import icosalattice.IcosahedronMath as icm
+# from icosalattice.GeneratePointCodes import get_random_point_code
 
 
 
 if __name__ == "__main__":
-    n_points = 100
-    usps = [UnitSpherePoint.random() for i in range(n_points)]
+    n_points = 100000
     pcs = []
-    for p in usps:
+    for i in range(n_points):
+        p = UnitSpherePoint.random()
+        xyz = p.xyz()
         spc, l, d = icm.get_peel_coordinates_of_point(p)
         pc = icm.get_point_code_from_peel_coordinates(spc, l, d)
         pcs.append(pc)
     els = [random.uniform(-100, 100) for i in range(n_points)]
     d = {pc: el for pc, el in zip(pcs, els)}
-    print(d)
     
+    for pc, el in d.items():
+        print(f"{pc},{el}")
